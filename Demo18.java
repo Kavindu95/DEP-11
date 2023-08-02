b2482a6420104388b6c33267af3aa542526de423
Kavindu Gayan Samarasinghe <115790355+Kavindu95@users.noreply.github.com>
2023-08-02 19:07:41 +0530

    aug02update

---
+++ Demo18.java
@@ -0,0 +1,11 @@
+public class Demo18 {
+    public static void main(String[] args) {
+        
+        short myShort=-10; 
+        char myChar=(char)myShort;
+        short myShort2=(short)myChar;
+        System.out.println(myShort2);
+
+        
+    }
+}
